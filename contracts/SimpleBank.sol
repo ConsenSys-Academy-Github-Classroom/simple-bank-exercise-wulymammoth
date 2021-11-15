edf53aee57e23dc231c0f648a6b29b65883b7d53
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2021-11-15 10:25:38 -0500

    deposit() and getBalance()

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -7,7 +7,7 @@
 pragma solidity >=0.5.16 <0.9.0;
 
 contract SimpleBank {
-  // State variables
+  /* State variables */
 
   // Fill in the visibility keyword.
   // Hint: We want to protect our users balance from other contracts
@@ -29,14 +29,13 @@ contract SimpleBank {
   event LogEnrolled(address indexed user);
 
   // Add 2 arguments for this event, an accountAddress and an amount
-  event LogDepositMade();
+  event LogDepositMade(address indexed accountAddress, uint amount);
 
   // Create an event called LogWithdrawal
   // Hint: it should take 3 arguments: an accountAddress, withdrawAmount and a newBalance
   event LogWithdrawal();
 
-  /* Functions
-   */
+  /* Functions */
 
   // Fallback function - Called if other functions don't match call or
   // sent ether without data
@@ -53,6 +52,7 @@ contract SimpleBank {
     // 1. A SPECIAL KEYWORD prevents function from editing state variables;
     //    allows function to run locally/off blockchain
     // 2. Get the balance of the sender of this transaction
+    return balances[msg.sender];
   }
 
   /// @notice Enroll a customer with the bank
@@ -60,7 +60,7 @@ contract SimpleBank {
   // Emit the appropriate event
   function enroll() public returns (bool) {
     // 1. enroll of the sender of this transaction
-    require(enrolled[msg.sender] == false, 'user already enrolled');
+    require(!enrolled[msg.sender], 'user already enrolled');
 
     enrolled[msg.sender] = true;
     emit LogEnrolled(msg.sender);
@@ -68,17 +68,21 @@ contract SimpleBank {
 
   /// @notice Deposit ether into bank
   /// @return The balance of the user after the deposit is made
-  function deposit() public returns (uint) {
+  function deposit() public payable returns (uint) {
     // 1. Add the appropriate keyword so that this function can receive ether
 
     // 2. Users should be enrolled before they can make deposits
+    require(enrolled[msg.sender], 'user must be enrolled to make a deposit');
 
     // 3. Add the amount to the user's balance. Hint: the amount can be
     //    accessed from of the global variable `msg`
+    balances[msg.sender] += msg.value;
 
     // 4. Emit the appropriate event associated with this function
+    emit LogDepositMade(msg.sender, msg.value);
 
     // 5. return the balance of sndr of this transaction
+    return balances[msg.sender];
   }
 
   /// @notice Withdraw ether from bank
